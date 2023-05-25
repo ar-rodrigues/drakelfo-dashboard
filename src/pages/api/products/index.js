a3a9bb8d6fc0c6733f7a6f44e7d51e5fb6ea59e5
ar-rodrigues <alissonrodrigues.st@gmail.com>
2023-05-25 10:27:25 -0600

    fixed path

--- src/pages/api/products/index.js
+++ src/pages/api/products/index.js
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth/next"
-import { authOptions } from "./auth/[...nextauth]"
+import { authOptions } from "../auth/[...nextauth]"
 import { getData } from "../operations/getData";
 
 export default async function handler(req, res) {
